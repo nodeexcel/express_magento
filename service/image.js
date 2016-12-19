c23f83aad3d13573bf2bb26c0904ba778e4dffe5
shekhar <shekhar@excellencetechnologies.in>
2016-12-19 15:39:53 +0530

    fiux issue in images.js

--- service/image.js
+++ service/image.js
@@ -92,51 +92,52 @@ resize = function (req, url, callback) {
 
 //FOR MINIFY IMAGE
 minify = function (req, url, callback) {
-    var APP_ID = req.headers.app_id;
-    if (url && APP_ID) {
-        var image_url = URL_.parse(url).path;
-        var app_id = APP_ID.replace(/[^a-zA-Z0-9 ]/g, "");
-        var filename = image_url.substring(0, image_url.lastIndexOf("/"));
-        var url_last_index_length = url.lastIndexOf('/');
-        var image_name = url.substring(url_last_index_length + 1);
-        var image_name_without_extension = image_name.substr(0, image_name.lastIndexOf('.'));
-        var image_jpg = '/' + image_name_without_extension + '.jpg';
-        var image_minified_name = filename.replace(app_id + "/", app_id + "/minify");
-        if (filename == '/default') {
-            callback(200, config.DEFAULT_IMAGE_URL);
-        } else {
-            sizeOf('public/original_image/' + image_name, function (err, dimensions) {
-                if (dimensions) {
-                    if (dimensions.width > 480 || dimensions.height > 240) {
-                        if (fileExists('public' + image_minified_name + '/' + image_jpg) == false && req.isAdmin == true) {
-                                 imagemin(["public/" + image_url], 'public' + image_minified_name, {
-                                  plugins: [
-                                  imageminMozjpeg(),
-                                  imageminPngquant({quality: '5'})
-                                  ]
-                                 }).then(files => {
-                                      if (files[0].path !== null) {
-                                          callback(200, config.CDN_URL+image_minified_name+image_jpg );
-                                      } else {
-                                          callback(500, config.DEFAULT_IMAGE_URL);
-                                      }
-                            })
-                        } else {
-                            if (fileExists('public/original_image/' + image_name) == false) {
-                                callback(200, url)
-                            } else {
-                                callback(200, config.CDN_URL + image_minified_name + image_jpg);
-                            }
-                        }
-                    } else {
-                        callback(200, config.CDN_URL + 'public/original_image/' + image_name);
-                    }
-                } else {
-                    callback(500, config.DEFAULT_IMAGE_URL);
-                }
-            });
-        }
-    } else {
-        callback(500, config.DEFAULT_IMAGE_URL);
-    }
+    return callback(200,url);
+    // var APP_ID = req.headers.app_id;
+    // if (url && APP_ID) {
+    //     var image_url = URL_.parse(url).path;
+    //     var app_id = APP_ID.replace(/[^a-zA-Z0-9 ]/g, "");
+    //     var filename = image_url.substring(0, image_url.lastIndexOf("/"));
+    //     var url_last_index_length = url.lastIndexOf('/');
+    //     var image_name = url.substring(url_last_index_length + 1);
+    //     var image_name_without_extension = image_name.substr(0, image_name.lastIndexOf('.'));
+    //     var image_jpg = '/' + image_name_without_extension + '.jpg';
+    //     var image_minified_name = filename.replace(app_id + "/", app_id + "/minify");
+    //     if (filename == '/default') {
+    //         callback(200, config.DEFAULT_IMAGE_URL);
+    //     } else {
+    //         sizeOf('public/original_image/' + image_name, function (err, dimensions) {
+    //             if (dimensions) {
+    //                 if (dimensions.width > 480 || dimensions.height > 240) {
+    //                     if (fileExists('public' + image_minified_name + '/' + image_jpg) == false && req.isAdmin == true) {
+    //                              imagemin(["public/" + image_url], 'public' + image_minified_name, {
+    //                               plugins: [
+    //                               imageminMozjpeg(),
+    //                               imageminPngquant({quality: '5'})
+    //                               ]
+    //                              }).then(files => {
+    //                                   if (files[0].path !== null) {
+    //                                       callback(200, config.CDN_URL+image_minified_name+image_jpg );
+    //                                   } else {
+    //                                       callback(500, config.DEFAULT_IMAGE_URL);
+    //                                   }
+    //                         })
+    //                     } else {
+    //                         if (fileExists('public/original_image/' + image_name) == false) {
+    //                             callback(200, url)
+    //                         } else {
+    //                             callback(200, config.CDN_URL + image_minified_name + image_jpg);
+    //                         }
+    //                     }
+    //                 } else {
+    //                     callback(200, config.CDN_URL + 'public/original_image/' + image_name);
+    //                 }
+    //             } else {
+    //                 callback(500, config.DEFAULT_IMAGE_URL);
+    //             }
+    //         });
+    //     }
+    // } else {
+    //     callback(500, config.DEFAULT_IMAGE_URL);
+    // }
 };
