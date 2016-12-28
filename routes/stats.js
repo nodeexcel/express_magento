b8d2e906d48bf94d9f857dc873de298fc88db878
shekhar <shekhar@excellencetechnologies.in>
2016-12-28 12:42:10 +0530

    statisctic api

--- routes/stats.js
+++ routes/stats.js
@@ -6,8 +6,8 @@ var moment = require('moment');
 router.post('/statistics_status', function (req, res) {
     var APP_ID = req.headers.app_id;
     staticsticAPIDB = req.staticsticAPIDB;
-    validate(req, {current_date: 'required'}, null, function (body) {
-        staticsticAPIDB.find({APP_ID: APP_ID, current_date: body.current_date}, function (err, dbrecord) {
+    validate(req, {current_date: 'required'}, null, function (body,result) {
+        staticsticAPIDB.find({APP_ID: APP_ID, current_date: result.current_date}, function (err, dbrecord) {
             if (err) {
                 oops(res, err);
             } else if (!dbrecord || dbrecord.length == '0') {
